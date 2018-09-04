ca5ddb018ab1d0c2f55d2e4b2473bbddbd0cdac3
Matt Borkowski <mattborkowski000@gmail.com>
2018-09-03 21:15:42 -0500

    setup basic functionality for set color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
+  color:string = 'rgba(255,255,255,1)';
   title = 'led';
+
+  onColorChange(){
+    let temp = this.color.split(",")
+    temp[0] = temp[0].split("(")[1];
+    console.log(temp);
+  }
 }
